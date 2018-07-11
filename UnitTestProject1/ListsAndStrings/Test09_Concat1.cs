17046af70e3e93f4def8e2969a9e2dbf1d38cf02
Kateryna Bodnarchuk <kateryna.bodnarchuk@hotmail.com>
2018-07-11 16:00:18 +0300

    Concat1 commit

--- UnitTestProject1/ListsAndStrings/Test09_Concat1.cs
+++ UnitTestProject1/ListsAndStrings/Test09_Concat1.cs
@@ -16,23 +16,19 @@ namespace UnitTestProject1.ListsAndStrings
             var result = new object[a.Length + b.Length];
             for (int i = 0; i < a.Length; i++)
             {
-                result[i] = a[i]; 
+                result[i] = a[i];
             }
-
             for (int i = 0; i < b.Length; i++)
             {
-                result[i] = b[i];
+                result[i + a.Length] = b[i];
             }
             return result;
         }
         [TestMethod]
         public void TestMethod1()
         {
-            List<object> a = new List<object>() { "a", "b", "c" };
-            List<object> b = new List<object>() { 1, 2, 3 };
-
-            Assert.IsTrue(MyConcat(a,b).Equals(a.Concat(b).ToString));
-
+            Assert.IsTrue(MyConcat(new object[] { "a", "b", "c" }, new object[] { 1, 2, 3 })
+                    .SequenceEqual(new object[] { "a", "b", "c", 1, 2, 3}));
         }
     }
 }
